6882914897affb24a60dc2f7a7466f3a79262afc
KnowinglyPossible <107905725+KnowinglyPossible@users.noreply.github.com>
2024-03-21 09:58:27 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const config = {
   challenge: false, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    isgay: 'jacob',
+    Interstellar: 'password',
   },
   routes: false, // Change this to false if you just want to host a bare server.
-  local: true, // Change this to false to disable local assets.
+  local: false, // Change this to false to disable local assets.
 }
 export default config
